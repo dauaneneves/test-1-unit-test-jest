aecdc4962e69acf803924ba3dd2780435e65e991
dauaneneves <dauanenevesgeron@gmail.com>
2025-04-08 21:58:01 -0300

    Ajustes dos arquivos necessários e desnecessarios

--- src/carro.js
+++ src/carro.js
@@ -1,20 +1,20 @@
 class Carro {
-    constructor(marca, modelo, ano) {
-        this.marca = marca;
-        this.modelo = modelo;
-        this.ano = ano;
-        this.kilometragem = 0;
-    }
+  constructor(marca, modelo, ano) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.ano = ano;
+    this.kilometragem = 0;
+  }
 
-    dirigir(distancia) {
-        if (distancia > 0) {
-            this.kilometragem += distancia;
-        }
+  dirigir(distancia) {
+    if (distancia > 0) {
+      this.kilometragem += distancia;
     }
+  }
 
-    obterInfo() {
-        return `${this.marca} ${this.modelo}, Ano: ${this.ano}, Quilometragem: ${this.kilometragem} km`;
-    }
+  obterInfo() {
+    return `${this.marca} ${this.modelo}, Ano: ${this.ano}, Quilometragem: ${this.kilometragem} km`;
+  }
 }
 
 module.exports = Carro;
